1aa5d94010be4a2b3b37171fab3159c413a2cbb3
devgiljong <calf-love@naver.com>
2023-11-12 01:11:02 +0900

    :sparkles: 퍼널 예제 완료

---
+++ composite-module/funnel/useFunnel.tsx
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useMemo } from 'react';
+import { NonEmptyArray, RouteFunnelProps, StepProps } from './Funnel.type';
+import { Funnel, Step } from './Funnel';
+
+export const useFunnel = <Steps extends NonEmptyArray<string>>(
+  array: Steps,
+  option?: {
+    initialStep: Steps[number];
+  },
+) => {
+  const [steps, setSteps] = React.useState<Steps>(array);
+  const router = useRouter();
+  const step = router.query.step as unknown as string;
+  const nextStep = (nextQuery: Steps[number]) => {
+    return () =>
+      router.push(`${router.pathname}?step=${nextQuery}`, undefined, {
+        shallow: true,
+      });
+  };
+  useEffect(() => {
+    if (option?.hasOwnProperty('initialStep')) {
+      router.replace(`${router.pathname}?step=${option.initialStep}`);
+    } else {
+      router.replace(`${router.pathname}?step=${steps[0]}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const FunnelComponent = useMemo(() => {
+    // eslint-disable-next-line react/display-name
+    return Object.assign(
+      (props: RouteFunnelProps<Steps>) => {
+        return <Funnel<Steps> step={step} steps={steps} {...props} />;
+      },
+      {
+        Step: (props: StepProps<Steps>) => {
+          return <Step {...props} />;
+        },
+      },
+    );
+  }, [step, steps]);
+  return [FunnelComponent, nextStep] as const;
+};
--- pages/funnel/index.tsx
+++ pages/funnel/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import { useFunnel } from '@/composite-module/funnel/useFunnel';
+import React from 'react';
 
-interface indexProps {
-  
-}
+interface indexProps {}
 
-const Index = ({}:indexProps) => {
+const Index = ({}: indexProps) => {
+  const route = ['iam', 'zzang', 'legend'] as const;
+  const [Funnel, nextStep] = useFunnel(route);
+  return (
+    <div>
+      <Funnel>
+        <Funnel.Step name="iam">
+          <div className="" onClick={nextStep('legend')}>
+            wow
+          </div>
+        </Funnel.Step>
+        <Funnel.Step name="zzang">
+          <div className="" onClick={nextStep('iam')}>
+            hi
+          </div>
+        </Funnel.Step>
+        <Funnel.Step name="legend">
+          <div className="" onClick={nextStep('zzang')}>
+            zzang im nida
+          </div>
+        </Funnel.Step>
+      </Funnel>
+    </div>
+  );
+};
 
- return <div>index</div>
-}
-
-export default Index
+export default Index;
