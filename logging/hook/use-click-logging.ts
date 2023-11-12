bfbd40811d09d92bd8345e165ade2410c18e5bd8
devgiljong <calf-love@naver.com>
2023-11-13 00:28:53 +0900

    :sparkles: 로그 실습

--- logging/hook/use-click-logging.ts
+++ logging/hook/use-click-logging.ts
@@ -1,5 +1,7 @@
 import React from 'react';
 import { UserTracker } from '../model/user-tracker';
+import { UserEventName } from '../model/user-event-name';
+import { UserEventPath } from '../model/user-event-path';
 
 export const useClickLogging = (): UserTracker => {
   const [user, setUser] = React.useState({
@@ -7,9 +9,39 @@ export const useClickLogging = (): UserTracker => {
     userGender: 'male',
     userName: '우은희',
   });
+
   return {
-    track: (eventName, eventPath, options) => {
-      console.log('이렇게 로그를 찍습니다.', eventName, eventPath, '순수하지 못한 기본 정보는 훅에서',user.userName);
+    track: (eventName, eventPath, option) => {
+      loggingFetch({ eventName, eventPath, option, user }).then((data) =>
+        console.log('서버응답', data),
+      );
+      console.log({ eventName, eventPath, option, user });
     },
   };
+};
+
+const loggingFetch = async ({
+  eventName,
+  eventPath,
+  user,
+  option,
+}: {
+  eventName: UserEventName;
+  eventPath: UserEventPath;
+  user: Record<string, unknown>;
+  option?: Record<string, unknown>;
+}) => {
+  const response = await fetch('/api/log/click', {
+    method: 'POST',
+    body: JSON.stringify({
+      eventDate: new Date(),
+      eventTime: new Date().toISOString(),
+      eventName: eventName,
+      eventPath: eventPath,
+      eventProperty: option || {},
+      eventUser: user,
+    }),
+  });
+  const data = await response.json();
+  return data;
 };
