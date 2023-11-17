dd0fa1a08f441f675c974c8ce985622dd8f23343
devgiljong <calf-love@naver.com>
2023-11-18 01:24:34 +0900

    :sparkles: 조이라이드 쓰는법 모르겠음 직접만드는게나을듯

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
+import React from 'react';
 
 export default function Home() {
-  return (
-    <div className=""> hello world</div>
-  )
+  const [client, setclient] = React.useState(false);
+  const [steps, setSteps] = React.useState([
+    {
+      target: '.my-first-step',
+      content: 'This is my awesome feature!',
+    },
+    {
+      target: '.my-other-step',
+      content: 'This another awesome feature!',
+    },
+  ]);
+  React.useEffect(() => {
+    setclient(true);
+  }, []);
+  return <div className=""></div>;
 }
+
