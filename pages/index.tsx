807af4cdfb697c526dd74ad885720636283e3ded
devgiljong <calf-love@naver.com>
2023-12-15 03:39:50 +0900

    :sparkles: 종속성추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useDatePicker } from '@xionhub/react-hook';
 
 export default function Home() {
+  const datepicker = useDatePicker(new Date());
+  console.log(datepicker);
   return <div className=""></div>;
 }
