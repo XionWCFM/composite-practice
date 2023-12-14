d73b79fd90ae27ef30e4a7a008421460e9456ffa
devgiljong <calf-love@naver.com>
2023-12-15 03:04:35 +0900

    :sparkles: Init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,5 @@
 import React from 'react';
 
 export default function Home() {
-  const [client, setclient] = React.useState(false);
-  const tourElementList = React.useRef<HTMLDivElement[]>([]);
-  const [tour, setTour] = React.useState({
-    tourOpen: false,
-    step: 0,
-  });
-
-  React.useEffect(() => {
-    setclient(true);
-  }, []);
-
-  return (
-    <div className="">
-      {client && (
-        <div className="">
-          {tour.tourOpen && (
-            <div className=" w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-25"></div>
-          )}
-          <div
-            className=" relative z-10 bg-white"
-            ref={(element) => (tourElementList.current[0] = element!)}
-          >
-            안녕하세요
-          </div>
-          <div
-            className=" relative bg-white z-10"
-            ref={(element) => (tourElementList.current[1] = element!)}
-          >
-            곽두팔입니다.
-          </div>
-        </div>
-      )}
-      <button
-        onClick={() => {
-          setTour((state) => ({
-            ...state,
-            tourOpen: !state.tourOpen,
-          }));
-        }}
-        className=" z-20"
-      >
-        이런 코드를 써버리는{' '}
-      </button>
-    </div>
-  );
+  return <div className=""></div>;
 }
--- pages/modal/index.tsx
+++ pages/modal/index.tsx
@@ -1,20 +1,9 @@
-import ModalContextExample from '@/components/modal/ModalContextExample';
-import { ModalContext } from '@/composite-module/modal/modal.context';
-import { useModal } from '@/composite-module/modal/use-modal';
 import React from 'react';
 
 interface indexProps {}
 
 const Index = ({}: indexProps) => {
-  const Modal = useModal();
-  return (
-    <div>
-      <ModalContext.Provider value={Modal}>
-        <ModalContextExample />
-        <Modal.Container />
-      </ModalContext.Provider>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Index;
