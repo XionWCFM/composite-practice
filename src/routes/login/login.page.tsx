d4761641a905380e9d47de67bbf7efd99dd9a706
devgiljong <calf-love@naver.com>
2023-12-29 10:26:29 +0900

    :sparkles: Add

--- src/routes/login/login.page.tsx
+++ src/routes/login/login.page.tsx
@@ -1,13 +1,11 @@
 import { certificationAtom } from '@/src/store/certification';
 import { useAtom } from 'jotai';
-import { useRouter } from 'next/router';
 import React from 'react';
 
 interface LoginPageProps {}
 
 const LoginPage = ({}: LoginPageProps) => {
   const [state, setState] = useAtom(certificationAtom);
-  const router = useRouter();
   return (
     <div>
       <button
@@ -16,7 +14,6 @@ const LoginPage = ({}: LoginPageProps) => {
             isLogin: !s.isLogin,
             accessToken: s.accessToken ? null : 'dsalkfmdkfdkl',
           }));
-          router.reload();
         }}
       >
         로그인하기
