e8e04b87212c9a310a429b3cf8f72263df6de207
devgiljong <calf-love@naver.com>
2023-12-31 12:17:20 +0900

    :sparkles: Allsettle 동작양식

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,19 @@ import WrapperConsumer from '@/src/components/consum/wrapper-consumer';
 import { CertificationWrapper } from '@/src/wrapper/certification';
 import React from 'react';
 export default function Home() {
+  React.useEffect(() => {
+    (async () => {
+      const a = await Promise.allSettled([
+        new Promise((res) => {
+          res('hi');
+        }),
+        new Promise((res, rej) => {
+          rej('bye');
+        }),
+      ]);
+      console.log(a);
+    })();
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center gap-y-2">
       <div className="">고차컴포넌트형식</div>
